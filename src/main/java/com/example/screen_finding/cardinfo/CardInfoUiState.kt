985d860359a1977da7974ac84b6b1bbc2672518f
Sarang Yang <>
2022-05-14 17:16:31 +0900

    update

--- src/main/java/com/example/screen_finding/cardinfo/CardInfoUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.screen_finding.cardinfo
-
-import com.example.torang_core.data.model.Restaurant
-
-data class CardInfoUiState(
-    val currentPosition: Int = 0,
-    val showCard: Boolean = false,
-    val restaurants: List<Restaurant> = ArrayList()
-)

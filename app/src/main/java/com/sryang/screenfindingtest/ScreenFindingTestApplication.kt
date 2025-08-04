3e2f601ebd74ac5ff1e91472a3ac608c18d23667
sryang <sarang628@gmail.com>
2025-08-05 07:27:45 +0800

    change package

--- app/src/main/java/com/sryang/screenfindingtest/ScreenFindingTestApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.sryang.screenfindingtest
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ScreenFindingTestApplication : Application()

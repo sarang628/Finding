1daa0e65e218ad59b7846f2a5a4ea80fe764101a
sryang <sarang628@gmail.com>
2025-08-12 07:19:35 +0800

    change location button
    - it is changed simple assited chip instead included permission and request location component parameter

--- library/src/main/java/com/example/screen_finding/ui/FindScreen.kt
+++ library/src/main/java/com/example/screen_finding/ui/FindScreen.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,15 +30,16 @@ import androidx.compose.ui.unit.sp
  * @param filter 필터
  * @param myLocation 내 위치 버튼
  * @param buttonBottomPadding 하단 패딩
+ * @param onMyLocation 내 위치 클릭
  */
 @Composable
-fun FindScreen(onZoomIn: () -> Unit, onZoomOut: () -> Unit, restaurantCardPage: @Composable () -> Unit, mapScreen: @Composable () -> Unit, filter: @Composable () -> Unit, myLocation: @Composable () -> Unit, buttonBottomPadding : Dp = 24.dp, onChangeRestaurantCardPageHeight : (Int) -> Unit = {}) {
+fun FindScreen(onZoomIn: () -> Unit, onZoomOut: () -> Unit, restaurantCardPage: @Composable () -> Unit, mapScreen: @Composable () -> Unit, filter: @Composable () -> Unit, buttonBottomPadding : Dp = 24.dp, onChangeRestaurantCardPageHeight : (Int) -> Unit = {}, onMyLocation : () -> Unit = {}) {
     Box {
         mapScreen.invoke() // 지도화면
         filter.invoke() // 필터화면
         Column(Modifier.align(Alignment.BottomCenter), verticalArrangement = Arrangement.Bottom, horizontalAlignment = Alignment.CenterHorizontally) {
             // 지도 확대, 축소, 내위치 버튼
-            Buttons(modifier = Modifier.padding(bottom = buttonBottomPadding), onZoomIn = onZoomIn, onZoomOut = onZoomOut, myLocation = myLocation)
+            Buttons(modifier = Modifier.padding(bottom = buttonBottomPadding), onZoomIn = onZoomIn, onZoomOut = onZoomOut, onMyLocation = onMyLocation)
             Box(modifier = Modifier.onSizeChanged{
                 onChangeRestaurantCardPageHeight.invoke(it.height)
             }) {restaurantCardPage.invoke()} // 카드 페이지
@@ -46,13 +50,13 @@ fun FindScreen(onZoomIn: () -> Unit, onZoomOut: () -> Unit, restaurantCardPage: 
 
 @Preview
 @Composable
-fun Buttons(modifier : Modifier = Modifier, onZoomIn: () -> Unit = {}, onZoomOut: () -> Unit = {}, myLocation: @Composable () -> Unit = {} ) {
+fun Buttons(modifier : Modifier = Modifier, onZoomIn: () -> Unit = {}, onZoomOut: () -> Unit = {}, onMyLocation : () -> Unit = {} ) {
     Row(modifier.padding(end = 8.dp )) {
 
         AssistChip(onZoomIn, label = {Text("+", fontSize = 30.sp)})
         Spacer(modifier = Modifier.width(8.dp))
         AssistChip(onZoomOut, label = { Text("-", fontSize = 30.sp) })
         Spacer(modifier = Modifier.width(8.dp))
-        myLocation.invoke()
+        AssistChip(onMyLocation, label = { Icon(Icons.Default.LocationOn, "") })
     }
 }

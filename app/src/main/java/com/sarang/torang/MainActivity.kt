7d166f6cd6d5c455bff20480f42571499720adec
sryang <sarang628@gmail.com>
2025-08-13 16:08:38 +0800

    adjust torangtheme

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -3,12 +3,18 @@ package com.sarang.torang
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sarang.torang.di.finding_di.FindingWithPermission
 import com.sryang.library.compose.workflow.BestPracticeViewModel
+import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,14 +22,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "finding") {
-                composable("finding") {
-                    val navController = RootNavController()
-                    FindingWithPermission(navController = navController, viewModel = BestPracticeViewModel())
-                }
-                composable("restaurant/{restaurantId}") {
-                    Text(text = "")
+            TorangTheme {
+                Surface(color = MaterialTheme.colorScheme.background){
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "finding") {
+                        composable("finding") {
+                            val navController = RootNavController()
+                            FindingWithPermission(navController = navController, viewModel = BestPracticeViewModel())
+                        }
+                        composable("restaurant/{restaurantId}") {
+                            Text(text = "")
+                        }
+                    }
                 }
             }
         }

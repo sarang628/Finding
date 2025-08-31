ffb6fda89b10456a8055ed32d85df1e4047d8d03
sryang <sarang628@gmail.com>
2025-08-31 14:14:39 +0800

    refactor : arrange code

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -5,10 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -16,19 +28,38 @@ import com.sarang.torang.di.finding_di.FindingWithPermission
 import com.sryang.library.compose.workflow.BestPracticeViewModel
 import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
+import com.sarang.torang.di.restaurant_list_bottom_sheet_di.CustomRestaurantItemImageLoader
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TorangTheme {
                 Surface(color = MaterialTheme.colorScheme.background){
                     val navController = rememberNavController()
+                    val scaffoldState: BottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+                    val coroutine = rememberCoroutineScope()
                     NavHost(navController = navController, startDestination = "finding") {
                         composable("finding") {
                             val navController = RootNavController()
-                            FindingWithPermission(navController = navController, viewModel = BestPracticeViewModel())
+                            CompositionLocalProvider(LocalRestaurantItemImageLoader provides CustomRestaurantItemImageLoader){
+                                Scaffold(floatingActionButton = {
+                                    FloatingActionButton({coroutine.launch { scaffoldState.bottomSheetState.expand() }}) { Icon(Icons.AutoMirrored.Default.List, "") }
+                                }) {
+                                    Box(Modifier.padding(it)){
+                                        RestaurantListBottomSheet(
+                                            scaffoldState = scaffoldState,
+                                            sheetPeekHeight = 0.dp,
+                                            modifier = Modifier.fillMaxSize()
+                                        ) {
+                                            FindingWithPermission(navController = navController, viewModel = BestPracticeViewModel())
+                                        }
+                                    }
+                                }
+                            }
                         }
                         composable("restaurant/{restaurantId}") {
                             Text(text = "")
